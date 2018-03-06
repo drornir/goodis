d1e26009bbfda83d853170e1fab932ac1085b375
DrorNir <dror@minutemedia.com>
2018-03-07 00:43:10 +0200

    setup tcp server

--- server/main.go
+++ server/main.go
@@ -1,19 +1,37 @@
 package server
 
+import (
+	"net"
+	"strconv"
+	"fmt"
+)
+
 type Server struct {
-	port int
+	host     string
+	port     string
+	listener net.Listener
 }
 
 func New(port int) *Server {
 	s := new(Server)
-	s.port = port
+	s.host = "localhost"
+	s.port = strconv.Itoa(port)
 	return s
 }
 
-func (s *Server) Listen() {
+func (s *Server) Listen() error {
 
+	addr := fmt.Sprintf("%v:%v", s.host, s.port)
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("tcp listen failed for address `%v`:\n%v", addr, err)
+	}
+	s.listener = l
+	return nil
 }
 
 func (s *Server) Close() {
-
+ 	_ = s.listener.Close()
+ 	s.listener = nil
 }
